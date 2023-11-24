a61324a106045c346fe5aa824ff99252706fed2a
Tulipreactjsmain <rdhnft@gmail.com>
2023-11-24 15:41:40 +0100

    Set Hero Content

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,45 @@
 import Image from "next/image";
-import { NavBarLogo } from ".";
+import { IoChevronDownSharp } from "react-icons/io5";
+import { Button } from "react-bootstrap";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
   return (
-    <div className="d-flex justify-content-between align-items-center position-fixed z-3 px-5 pt-4">
-      <div>
+    <div className="d-flex justify-content-between align-items-center w-100 position-fixed z-3 px-2 px-md-5 px-lg-5 pt-2 pt-md-4 pt-lg-4">
+      <div className="fade-right-animation delay-1">
         <Image
           src="https://res.cloudinary.com/techbro/image/upload/v1700607786/pngfilewhite_rkaopf.svg"
           width={185}
           height={57}
+          className="logo"
           alt="Stardelite logo"
-          priority={false} 
+          priority={false}
+          placeholder="blur"
+          blurDataURL={
+            "https://res.cloudinary.com/techbro/image/upload/v1700607786/pngfilewhite_rkaopf.svg"
+          }
         />
-        {/* <NavBarLogo className="bg-success p-0 m-0" style={{width:"300px", height:"300px"}}/> */}
       </div>
+      <div
+        className="text-white d-none d-md-flex d-lg-flex gap-4 justify-content-center align-items-center"
+        style={{ cursor: "pointer" }}
+      >
+        <span>
+          Services <IoChevronDownSharp color="#ffcc00" />
+        </span>
+        <span>
+          Technologies <IoChevronDownSharp color="#ffcc00" />
+        </span>
+        <span>
+          About <IoChevronDownSharp color="#ffcc00" />
+        </span>
+        <div className="navBtnBorder">
+          <Button type="button" className="btn navBtn  text-black">
+            Contact us
+          </Button>
+        </div>
+      </div>
+      <AiOutlineMenu className="text-white fs-1 d-md-none d-lg-none"/>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,34 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Layout } from "@/components";
+import React, { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 const inter = Inter({ subsets: ["latin"] });
+const images = [
+  "https://images.unsplash.com/photo-1554050857-c84a8abdb5e2?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D",
+  "https://res.cloudinary.com/techbro/image/upload/v1700738619/unsplash_DBGb9u1Yf6Q_zham6v.svg",
+  "https://res.cloudinary.com/techbro/image/upload/v1700739951/unsplash_Xk4hnBZHHZ0_azpwb1.svg",
+  "https://res.cloudinary.com/techbro/image/upload/v1700832852/Frame_1_ryctrp.svg",
+  "https://res.cloudinary.com/techbro/image/upload/v1700832853/Rectangle_3_gyva6x.svg",
+  "https://res.cloudinary.com/techbro/image/upload/v1700832855/Rectangle_4_nr5rqr.svg",
+  "https://res.cloudinary.com/techbro/image/upload/v1700832824/Pexels_Photo_by_Gantas_Vaic%CC%8Ciule%CC%87nas_vkcm6c.svg",
+];
 
 export default function Home() {
+  const [backgroundImage, setBackgroundImage] = useState(images[0]);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = images[1];
+    image.onload = () => {
+      setBackgroundImage(image.src);
+    };
+    return () => {
+      image.onload = null;
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,26 +36,55 @@ export default function Home() {
         <meta
           name="description"
           content="Elevate your ideas and reach for the 
-          
-          
           stars with Stardelite. We are more than a software development company; we're your partners in innovation. Transform your vision into reality through cutting-edge technology and personalized solutions. Join a community of dreamers and achievers. Let's propel your business to new heights together!"
         />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <section
-          className="w-100 bg-black overlay"
+          className="w-100 bg-black position-relative"
           style={{
             height: "100vh",
           }}
         >
-          <div
-            className="Hero-BG w-100 p-0 m-0 slide-in-blurred-top"
-            style={{
-              height: "100vh",
-            }}
-          ></div>
-
+          <Carousel
+            fade
+            controls={false}
+            indicators={false}
+            className="Hero-BG"
+          >
+            <Carousel.Item className="Hero-BG">
+              <div
+                className="Hero-BG"
+                style={{ backgroundImage: `url('${backgroundImage}')` }}
+              ></div>
+            </Carousel.Item>
+            <Carousel.Item className="Hero-BG">
+              <div
+                className="Hero-BG"
+                style={{ backgroundImage: `url('${images[2]}')` }}
+              ></div>
+            </Carousel.Item>
+            <Carousel.Item className="Hero-BG">
+              <div
+                className="Hero-BG"
+                style={{ backgroundImage: `url('${images[3]}')` }}
+              ></div>
+            </Carousel.Item>
+            <Carousel.Item className="Hero-BG">
+              <div
+                className="Hero-BG"
+                style={{ backgroundImage: `url('${images[4]}')` }}
+              ></div>
+            </Carousel.Item>
+          </Carousel>
+          <div className="heroContent text-white">
+            <div className="d-flex justify-content-center  fs-1 align-items-center w-100 h-100">
+              <p className="" style={{width:"500px !important"}}>Software Development</p>
+            </div>
+          </div>
+          
         </section>
+        <section style={{width:"500px", height:"500px"}}></section>
       </main>
     </>
   );
